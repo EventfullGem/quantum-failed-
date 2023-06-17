3a17a629b74ad9c0ea0f5d508c4543e77eb6f8fa
EventfullGem <82291575+EventfullGem@users.noreply.github.com>
2023-06-16 22:54:46 -0400

    ygh

--- quamtum.py
+++ quamtum.py
@@ -1,35 +1,4 @@
-from qiskit import QuantumCircuit, transpile
-from qiskit_aer import AerSimulator
+from qiskit import QuantumCircuit, assemble, Aer
 from qiskit.visualization import plot_histogram
-
-# Use Aer's AerSimulator
-simulator = AerSimulator()
-
-# Create a Quantum Circuit acting on the q register
-circuit = QuantumCircuit(2, 2)
-
-# Add a H gate on qubit 0
-circuit.h(0)
-
-# Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-circuit.cx(0, 1)
-
-# Map the quantum measurement to the classical bits
-circuit.measure([0, 1], [0, 1])
-
-# Compile the circuit for the support instruction set (basis_gates)
-# and topology (coupling_map) of the backend
-compiled_circuit = transpile(circuit, simulator)
-
-# Execute the circuit on the aer simulator
-job = simulator.run(compiled_circuit, shots=1000)
-
-# Grab results from the job
-result = job.result()
-
-# Returns counts
-counts = result.get_counts(compiled_circuit)
-print("\nTotal count for 00 and 11 are:", counts)
-
-# Draw the circuit
-circuit.draw("mpl")
+from qiskit_textbook.widgets import binary_widget
+binary_widget(nbits=5)
